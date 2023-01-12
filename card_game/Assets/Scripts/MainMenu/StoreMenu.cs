87b1c6e3dbf9055d2de926d360143f4c18a7b976
Abzaparov-Nikolay <nd.abzaparov@gmail.com>
2023-01-12 19:22:38 +0500

    Добавил локализацию(eng + ru)

--- card_game/Assets/Scripts/MainMenu/StoreMenu.cs
+++ card_game/Assets/Scripts/MainMenu/StoreMenu.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
 using YG;
 
 public class StoreMenu : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI GoldAmountText;
+    [SerializeField] private LocalizedString localGoldAmount;
+
     void Start()
     {
         SaveDataManager.Instance.GoldChanged += UpdatePlayerStashText;
@@ -13,11 +16,25 @@ public class StoreMenu : MonoBehaviour
 
     private void OnEnable()
     {
+        localGoldAmount.StringChanged += UpdateLocalGoldAmount;
+        localGoldAmount.Arguments = new object[1] { SaveDataManager.Instance.GoldAmount };
         UpdatePlayerStashText();
+    }
+
+    private void OnDisable()
+    {
+        localGoldAmount.StringChanged -= UpdateLocalGoldAmount;
     }
 
     private void UpdatePlayerStashText()
     {
-        GoldAmountText.text = $"Gold : {SaveDataManager.Instance.GoldAmount}";
+        localGoldAmount.Arguments[0] = SaveDataManager.Instance.GoldAmount;
+        localGoldAmount.RefreshString();
+        //GoldAmountText.text = $"Gold : {SaveDataManager.Instance.GoldAmount}";
+    }
+
+    private void UpdateLocalGoldAmount(string s)
+    {
+        GoldAmountText.text = s;
     }
 }
